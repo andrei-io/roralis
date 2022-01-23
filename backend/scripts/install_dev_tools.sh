345538c56d16297302321033b62af4b0b8aec0fe
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2022-01-23 13:10:59 +0200

    general: update install script

--- backend/scripts/install_dev_tools.sh
+++ backend/scripts/install_dev_tools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~
 go install github.com/pressly/goose/v3/cmd/goose@latest
 go install github.com/go-swagger/go-swagger/cmd/swagger@latest
+curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0
