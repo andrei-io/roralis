281817096c72ab7506a095ec65d641c46304b5fd
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2022-02-20 21:12:27 +0200

    backend: docker image

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"errors"
+	"os"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -14,13 +18,23 @@ func Load() error {
 	viper.SetDefault("PORT", "8080")
 	viper.SetDefault("GIN_MODE", "release")
 	viper.SetDefault("DB_URL", "host=localhost user=admin password=admin dbname=asdf sslmode=disable")
+	viper.SetDefault("SENDGRID_KEY", "")
+	viper.SetDefault("EMAIL_NAME", "")
+	viper.SetDefault("EMAIL_FROM", "")
+	viper.SetDefault("ENV", "")
+	viper.SetDefault("REDIS_ADDRESS", "")
+	viper.SetDefault("REDIS_PASSWORD", "")
+	viper.SetDefault("JWT_PUBLIC", "")
+	viper.SetDefault("JWT_PRIVATE", "")
 
-	viper.SetConfigFile(".env")
-	viper.SetConfigType("props")
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return err
+	_, err = os.Stat(".env")
+	if !errors.Is(err, os.ErrNotExist) {
+		viper.SetConfigFile(".env")
+		viper.SetConfigType("props")
+		err = viper.ReadInConfig()
+		if err != nil {
+			return err
+		}
 	}
 
 	// Override config parameters from environment variables if specified
@@ -29,5 +43,20 @@ func Load() error {
 			return err
 		}
 	}
+
+	// Some .env readers dont read things properly
+
+	key := viper.GetString("JWT_PRIVATE")
+
+	key = strings.ReplaceAll(key, `\n`, "\n")
+
+	viper.Set("JWT_PRIVATE", key)
+
+	key = viper.GetString("JWT_PUBLIC")
+
+	key = strings.ReplaceAll(key, `\n`, "\n")
+
+	viper.Set("JWT_PUBLIC", key)
+
 	return nil
 }
