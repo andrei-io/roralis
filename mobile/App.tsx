10aa9dfdbb271a4f23ac9cfb9a4b0efaf7a0449f
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2022-03-01 19:41:24 +0200

    mobile: set up type aliases + load font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,13 @@
+import { Inter_600SemiBold, useFonts } from '@expo-google-fonts/inter';
 import { Router } from './router/router';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Router />;
 }
