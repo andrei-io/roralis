861589748fa316381531e36b88e04bca93548c5b
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2022-05-16 16:50:13 +0300

    mobile: deploy mobile app(standalone apk)

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -7,6 +7,7 @@ export default {
   slug: 'roralis',
   version: '1.0.0',
   orientation: 'portrait',
+  icon: 'assets/icon-circle.png',
 
   updates: {
     fallbackToCacheTimeout: 0,
@@ -19,5 +20,9 @@ export default {
   extra: {
     production: process.env.PRODUCTION_BUILD === '1',
     initialScreenName: process.env.INITIAL_SCREEN_NAME ?? 'Home',
+  },
+  android: {
+    package: 'com.giveit.roralis',
+    versionCode: 1,
   },
 };
